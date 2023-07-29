f39533c512576712b47c33861239e0de9d26414d
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-07-30 00:02:29 +0100

    create profile component

--- my-free-space/src/app/core/header/header.component.ts
+++ my-free-space/src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/shared/services/user.service';
+import { IUser } from 'src/app/shared/types/user';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,21 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  constructor(private userService:UserService) {}
 
-  get isLogged():boolean{
+  userGender: string = '';
+
+  constructor(private userService: UserService) {
+    userService.getUser().subscribe({
+      next: (u) => {
+        this.userGender = u.gender;
+      },
+      error: (err) => {
+        this.userGender = '';
+      },
+    });
+  }
+
+  get isLogged(): boolean {
     return this.userService.isLogged;
   }
 }
--- my-free-space/src/app/features/auth/profile/profile.component.ts
+++ my-free-space/src/app/features/auth/profile/profile.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
+import { IUser } from 'src/app/shared/types/user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  user: IUser | undefined;
 
+  constructor(private userService: UserService) {
+    this.userService.getUser().subscribe({
+      next: (u) => {
+        this.user = u;
+      },
+      error: () => {
+        this.user = undefined;
+      },
+    });
+  }
 }
