7b2400b7416551e809bbfa39ab47b72e388bb8cf
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-08-11 11:19:27 +0100

    fix bug in logout - clear search after logout

--- my-free-space/src/app/features/auth/logout/logout.component.ts
+++ my-free-space/src/app/features/auth/logout/logout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { USER_KEY } from 'src/app/shared/costants';
+import { SearchService } from 'src/app/shared/services/searchParams.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
@@ -9,23 +10,17 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./logout.component.css'],
 })
 export class LogoutComponent implements OnInit {
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private searchService: SearchService
+  ) {}
 
   ngOnInit(): void {
-    this.userService.logout().subscribe({
-      next: () => {
-        localStorage.removeItem(USER_KEY);
-        this.router.navigate(['/home']);
-      },
-      error: (err) => {
-        console.log(err);
-        localStorage.removeItem(USER_KEY);
-        this.router.navigate(['/home']);
-      },
-      complete: () => {
-        localStorage.removeItem(USER_KEY);
-        this.router.navigate(['/home']);
-      },
+    this.userService.logout().subscribe(() => {
+      localStorage.removeItem(USER_KEY);
+      this.router.navigate(['/home']);
+      this.searchService.clearSearch();
     });
   }
 }
