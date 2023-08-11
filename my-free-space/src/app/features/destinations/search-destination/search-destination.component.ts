511bf68d0be73713731fc98636e6adb4624b3677
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-08-11 12:24:15 +0100

    add redirect after search button pressed

--- my-free-space/src/app/features/destinations/search-destination/search-destination.component.ts
+++ my-free-space/src/app/features/destinations/search-destination/search-destination.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { COUNTRIES_LIST } from 'src/app/shared/costants';
 import { SearchService } from 'src/app/shared/services/searchParams.service';
 
@@ -11,15 +12,26 @@ import { SearchService } from 'src/app/shared/services/searchParams.service';
 export class SearchDestinationComponent {
   countries: string[] = COUNTRIES_LIST;
 
-  constructor(private serachService: SearchService) {}
+  clearPressed: boolean = false;
+
+  constructor(private serachService: SearchService, private router: Router) {}
 
   getParams(form: NgForm): void {
     const { name, country } = form.value;
+
     this.serachService.changeParams(name, country);
+
+    if (this.clearPressed) {
+      this.clearPressed = false;
+      return;
+    }
+
+    this.router.navigate(['/dest/posts-list']);
   }
 
   clearSearch(form: NgForm): void {
     form.resetForm({ name: '', country: '' });
     this.serachService.clearSearch();
+    this.clearPressed = true;
   }
 }
