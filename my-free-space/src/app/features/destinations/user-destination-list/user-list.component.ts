ac13f1a6ca1f40f6f25f1d18148e1aea220351c6
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-10-25 23:41:43 +0100

    improve user destination service to not use search service

--- my-free-space/src/app/features/destinations/user-destination-list/user-list.component.ts
+++ my-free-space/src/app/features/destinations/user-destination-list/user-list.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, combineLatest, switchMap } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 import { DestinationService } from 'src/app/shared/services/destination.service';
-import { SearchService } from 'src/app/shared/services/search.service';
-import { UserService } from 'src/app/shared/services/user.service';
 import { IDestination } from 'src/app/shared/types/destination';
 
 @Component({
@@ -11,68 +8,31 @@ import { IDestination } from 'src/app/shared/types/destination';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent implements OnInit, OnDestroy {
+export class UserListComponent implements OnInit {
   list: IDestination[] = [];
-  page: number = this.searchService.getUserListPage;
+  // page: number = this.searchService.getUserListPage;
   countDest: number = 0;
   lastPage: number = 0;
   loading: boolean = true;
-  private subscription: Subscription | undefined;
 
   constructor(
-    private userService: UserService,
     private destinationService: DestinationService,
-    private searchService: SearchService
   ) {}
 
   ngOnInit(): void {
-    this.searchService.params$.subscribe(() => {
-      this.loading = true;
+    this.destinationService.getUserDestinations(0).subscribe((list) => {
+      this.list = list;
+      this.loading = false;
     });
-    this.subscribeToMultipleObservables();
   }
 
-  subscribeToMultipleObservables(): void {
-    const searchObs = this.searchService.params$;
-    const userPageObs = this.searchService.userListPage$;
-    this.loading = true;
+  // increaseUserListPage() {
+  //   this.loading = true;
+  //   this.searchService.setUserListPage(this.page + 1);
+  // }
 
-    this.subscription = combineLatest([userPageObs, searchObs])
-      .pipe(
-        switchMap(([pN, data]) => {
-          this.page = pN;
-          this.loading = true;
-          
-          return combineLatest([
-            this.getListDestinations(data.name, data.country),
-          ]);
-        })
-      )
-      .subscribe(([destinations]) => {
-        this.list = destinations;
-        this.loading = false;
-      });
-  }
-
-  private getListDestinations(name: string, country: string) {
-    return this.destinationService.getUserDestinations(
-      name,
-      country,
-      this.page
-    );
-  }
-
-  increaseUserListPage() {
-    this.loading = true;
-    this.searchService.setUserListPage(this.page + 1);
-  }
-
-  decreaseUserListPage() {
-    this.loading = true;
-    this.searchService.setUserListPage(this.page - 1);
-  }
-
-  ngOnDestroy() {
-    this.subscription?.unsubscribe();
-  }
+  // decreaseUserListPage() {
+  //   this.loading = true;
+  //   this.searchService.setUserListPage(this.page - 1);
+  // }
 }
