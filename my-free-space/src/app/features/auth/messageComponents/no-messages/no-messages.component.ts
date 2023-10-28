ff811b33214efc9420bcbf1215d41e4a6d3071e3
BlagovestPanayotov <83649155+BlagovestPanayotov@users.noreply.github.com>
2023-10-28 23:05:06 +0100

    create no message component

---
+++ my-free-space/src/app/features/auth/messageComponents/no-messages/no-messages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-messages',
+  templateUrl: './no-messages.component.html',
+  styleUrls: ['./no-messages.component.css']
+})
+export class NoMessagesComponent {
+
+}
